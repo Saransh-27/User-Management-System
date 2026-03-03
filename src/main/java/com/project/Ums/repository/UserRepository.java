4043bbcb4fda07dae3ef3f5e7ff5ebb4e2fbed42
Saransh-27 <saranshdhiman353@gmail.com>
2026-03-03 20:04:27 +0530

    Fixing some return type

--- src/main/java/com/project/Ums/repository/UserRepository.java
+++ src/main/java/com/project/Ums/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.project.Ums.repository;
 
 import com.project.Ums.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
 
 
 public interface UserRepository extends MongoRepository<User, String> {
     void deleteUserByUserName(String username);
-    User findByUserName(String userName);
+    Optional<User> findByUserName(String userName);
+    Optional<User> findByIdAndEmail(String id, String email);
 }
