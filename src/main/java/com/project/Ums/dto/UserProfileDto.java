998d3fd1af0496cc80cfe902460f382175fa512d
Saransh-27 <saranshdhiman353@gmail.com>
2026-03-03 20:05:40 +0530

    Adding one dto for publicController

---
+++ src/main/java/com/project/Ums/dto/UserProfileDto.java
@@ -0,0 +1,20 @@
+package com.project.Ums.dto;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserProfileDto {
+    private String id;
+    private String userName;
+    private String email;
+    private String password;
+    private List<String> roles = new ArrayList<>();
+    private String status;
+}
