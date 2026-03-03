1e16d43b7d36ebe9e2b2f71c2260fdc2cdc979b9
Saransh-27 <saranshdhiman353@gmail.com>
2026-03-03 20:13:42 +0530

    Adding an Endpoint for otp verfication

--- src/main/java/com/project/Ums/controller/AuthController.java
+++ src/main/java/com/project/Ums/controller/AuthController.java
@@ -1,16 +1,17 @@
 package com.project.Ums.controller;
 
 import com.project.Ums.dto.LoginDto;
+import com.project.Ums.entity.User;
+import com.project.Ums.repository.UserRepository;
+import com.project.Ums.service.OtpService;
 import com.project.Ums.utils.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -20,9 +21,20 @@ public class AuthController {
     private JwtUtil jwtUtil;
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private OtpService otpService;
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/login")
     public String login(@RequestBody LoginDto dto){
         try{
+            User user = userRepository.findByUserName(dto.getUserName())
+                        .orElseThrow(() -> new RuntimeException("User not found"));
+            
+            if(!"ACTIVE".equals(user.getStatus())){
+                throw new RuntimeException("User is not verified");
+            }
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(dto.getUserName(), dto.getPassword()));
             return jwtUtil.generateToken(authentication.getName());
@@ -30,5 +42,14 @@ public class AuthController {
             log.error("Exception occurred while createAuthenticationToken ", e);
             throw new RuntimeException("Incorrect username or password", e);
         }
+    }
+
+    @PostMapping("/verify-otp")
+    public ResponseEntity<String> verifyOtp(
+            @RequestParam String id, 
+            @RequestParam String email, 
+            @RequestParam String otp) {
+        String response = otpService.verifyOtp(id, email, otp);
+        return ResponseEntity.ok(response);
     }
 }
