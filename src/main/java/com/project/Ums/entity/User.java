43d0dcec99bda4b508444f367f45c2133c11bcd6
Saransh-27 <saranshdhiman353@gmail.com>
2026-03-11 19:40:15 +0530

    Addinf Indexing in the feilds for fast access

--- src/main/java/com/project/Ums/entity/User.java
+++ src/main/java/com/project/Ums/entity/User.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -19,11 +20,15 @@ import java.util.List;
 public class User {
     @Id
     private String id;
+    @Indexed
     private String userName;
+//    @Indexed(unique = true)
     private String email;
     private String password;
     private List<String> roles = new ArrayList<>();
+    @Indexed
     private String status;
+    @Indexed
     private String otp;
     private LocalDateTime otpExpiry;
 }
