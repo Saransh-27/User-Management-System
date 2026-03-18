04594915ae75e795f15ef53d151f6a95c2a4e8a0
Saransh-27 <saranshdhiman353@gmail.com>
2026-03-18 18:44:29 +0530

    Repository Additions: Added search methods for users in UserRepository

--- src/main/java/com/project/Ums/repository/UserRepository.java
+++ src/main/java/com/project/Ums/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.project.Ums.repository;
 
 import com.project.Ums.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -9,4 +10,9 @@ public interface UserRepository extends MongoRepository<User, String> {
     void deleteUserByUserName(String username);
     Optional<User> findByUserName(String userName);
     Optional<User> findByIdAndEmail(String id, String email);
+    
+    // Search methods
+    List<User> findByUserNameContainingIgnoreCase(String userName);
+    List<User> findByEmailContainingIgnoreCase(String email);
+    List<User> findByUserNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String userName, String email);
 }
