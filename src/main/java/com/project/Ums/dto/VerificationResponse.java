185e126a7a1cfb5a41b51941b66b3e244d78dbdd
Saransh-27 <saranshdhiman353@gmail.com>
2026-03-21 19:28:51 +0530

    Add verification response DTO

---
+++ src/main/java/com/project/Ums/dto/VerificationResponse.java
@@ -0,0 +1,14 @@
+package com.project.Ums.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerificationResponse {
+    private boolean success;
+    private String message;
+    private String error;
+}
