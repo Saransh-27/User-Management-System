9a7c4cd8769604bc5c50bf50698d662837ee5bb6
Saransh-27 <saranshdhiman353@gmail.com>
2026-03-25 18:00:18 +0530

    Add reset password request DTO with token and password validation

---
+++ src/main/java/com/project/Ums/dto/ResetPasswordDto.java
@@ -0,0 +1,20 @@
+package com.project.Ums.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResetPasswordDto {
+    @NotBlank(message = "Token is required")
+    private String token;
+    
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String newPassword;
+}
