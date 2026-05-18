f8703926508506a5f919d7bad6acdb7b498988c3
unknown <saranshdhiman353@gmail.com>
2026-05-18 18:12:50 +0530

    Adding a custom exception file and updating .yml file

---
+++ src/main/java/com/project/Ums/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.project.Ums.exception;
+
+public class FileStorageException extends RuntimeException{
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
